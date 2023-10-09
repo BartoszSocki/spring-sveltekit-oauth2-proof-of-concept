cc9b2a7b8403c61a89ee9297e325cebec7a79322
bartosz <bartosz.socki@gmail.com>
2023-10-09 20:20:57 +0200

    tables for AS

---
+++ postgres/02-authorization-server-db.init.sql
@@ -0,0 +1,38 @@
+create database authorization_server_db;
+
+\c authorization_server_db;
+
+create schema authorization_server;
+create user authorization_server with password 'Password1!';
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+create table if not exists authorization_server.users (
+	id serial primary key,
+	email text not null unique,
+	password text null
+);
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+create table if not exists authorization_server.roles (
+	id serial primary key,
+	name text not null unique
+);
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+create table if not exists authorization_server.user_role (
+	id serial primary key,
+	user_id int not null,
+	role_id int not null, 
+
+	constraint fk_user_id foreign key (user_id) references authorization_server.users(id),
+	constraint fk_role_id foreign key (role_id) references authorization_server.roles(id)
+);
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+grant all privileges on schema authorization_server to authorization_server;
+grant all privileges on database authorization_server_db to authorization_server;
+grant all privileges on all tables in schema authorization_server to authorization_server;
