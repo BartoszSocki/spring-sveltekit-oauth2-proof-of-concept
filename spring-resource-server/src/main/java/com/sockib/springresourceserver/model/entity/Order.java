738e9f581cf78e1c4e5c487695fb03a69a2fcfbb
bartosz <bartosz.socki@gmail.com>
2023-10-19 14:53:08 +0200

    added: annotations to order

--- spring-resource-server/src/main/java/com/sockib/springresourceserver/model/entity/Order.java
+++ spring-resource-server/src/main/java/com/sockib/springresourceserver/model/entity/Order.java
@@ -4,16 +4,31 @@ import com.sockib.springresourceserver.model.valueobject.Address;
 import com.sockib.springresourceserver.model.valueobject.OrderStatus;
 import com.sockib.springresourceserver.model.valueobject.Price;
 import com.sockib.springresourceserver.model.entity.mappedsuperclass.Resource;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
 
-//@Entity
+@Getter
+@Setter
+
+@Entity
 public class Order extends Resource {
 
-    // TODO: add required annotations
-    private ProductStock product;
+    @ManyToOne
+    private Product product;
     private Price price;
+
+    @ManyToOne
     private User seller;
+
+    @ManyToOne
     private User buyer;
     private Address address;
+
+    @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus;
 
 }
