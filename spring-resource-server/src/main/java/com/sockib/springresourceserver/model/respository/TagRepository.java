0dae1676a0f77a02c2ac792a52dc2ce2a16459a9
bartosz <bartosz.socki@gmail.com>
2023-10-26 20:12:26 +0200

    changed method signature

--- spring-resource-server/src/main/java/com/sockib/springresourceserver/model/respository/TagRepository.java
+++ spring-resource-server/src/main/java/com/sockib/springresourceserver/model/respository/TagRepository.java
@@ -3,10 +3,11 @@ package com.sockib.springresourceserver.model.respository;
 import com.sockib.springresourceserver.model.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
 
-    List<Tag> findAllByNameIn(Iterable<String> names);
+    List<Tag> findAllByNameIn(Collection<String> name);
 
 }
