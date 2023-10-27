06dec3d20aced626f951d3c54bf0639de8987429
bartosz <bartosz.socki@gmail.com>
2023-10-27 19:18:45 +0200

    added new method to product search repository for sorting

--- spring-resource-server/src/main/java/com/sockib/springresourceserver/model/respository/SearchableProductRepository.java
+++ spring-resource-server/src/main/java/com/sockib/springresourceserver/model/respository/SearchableProductRepository.java
@@ -2,6 +2,7 @@ package com.sockib.springresourceserver.model.respository;
 
 import com.sockib.springresourceserver.model.entity.Product;
 import com.sockib.springresourceserver.util.search.Page;
+import com.sockib.springresourceserver.util.search.Sort;
 import com.sockib.springresourceserver.util.search.Specification;
 import org.springframework.data.domain.Pageable;
 
@@ -11,5 +12,7 @@ public interface SearchableProductRepository {
 
     List<Product> findProducts(Specification<Product> specification, Page page, String entityGraphName);
     List<Product> findProducts(Specification<Product> specification, Page page);
+
+    List<Product> findProducts(Specification<Product> specification, Page page, Sort sort, String entityGraphName);
 
 }
