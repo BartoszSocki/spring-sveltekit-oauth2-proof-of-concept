9fa679b46865373a31f931bf3bb363feaaf792d6
bartosz <bartosz.socki@gmail.com>
2023-10-28 23:56:01 +0200

    added missing files

---
+++ sveltekit/src/lib/Filter/Filter.svelte
@@ -0,0 +1,10 @@
+<div>
+    filter
+</div>
+
+<style>
+    div {
+        padding: 1rem;
+        border: 1px solid black;
+    }
+</style>
---
+++ sveltekit/src/lib/Product/Product.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let product
+    console.log({product})
+</script>
+
+<section>
+    <p>this is product</p>
+    <p>{product.name}</p>
+    <!-- <p>{product.score}</p> -->
+</section>
+
+<style>
+    section {
+        padding: 1rem;
+        border: 1px solid gray;
+    }
+</style>
