5af656ccd73943f1f1f9db3891a6ea794f976f52
bartosz <bartosz.socki@gmail.com>
2023-10-30 17:41:13 +0100

    added simple pagination (probably needs rewrite already)

--- sveltekit/src/routes/product/list/+page.js
+++ sveltekit/src/routes/product/list/+page.js
@@ -1,13 +1,21 @@
 export function _SearchProductsVariables({ url }) {
     let filters = [];
     for (const [key, value] of url.searchParams.entries()) {
-        filters.push(paramToFilterConverter(key, value))
-    } 
+        const filter = paramToFilterConverter(key, value)
+        if (filter !== undefined && filter !== null) {
+            filters.push(filter)
+        }
+    }
+
+    let offset = parseInt(url.searchParams.get('offset') ?? '0') ?? 0;
+    let limit = parseInt(url.searchParams.get('limit') ?? '10') ?? 10;
+    let sortField = url.searchParams.get('sort') ?? 'name';
+    let sortDir = url.searchParams.get('sortDir') ?? "ASC";
 
     return {
         filters,
-        page: page(0, 10),
-        sort: sort('name', 'ASC')
+        page: page(offset, limit),
+        sort: sort(sortField, sortDir)
     }
 }
 
--- sveltekit/src/routes/product/list/+page.svelte
+++ sveltekit/src/routes/product/list/+page.svelte
@@ -1,9 +1,24 @@
 <script>
 	import Product from "$lib/Product/Product.svelte";
+    import { page } from '$app/stores';
 
     export let data;
 
+    $: currentOffset = parseInt($page.url.searchParams.get('offset'))
+    $: currentLimit = parseInt($page.url.searchParams.get('limit'))
+
+    let nextUrl, prevUrl;
+
+    $: {
+        let url = $page.url
+        url.searchParams.set('offset', currentOffset + currentLimit);
+        nextUrl = url.href;
+        url.searchParams.set('offset', Math.max(0, currentOffset - currentLimit));
+        prevUrl = url.href;
+    } 
+
     $: ({SearchProducts} = data);
+    // $: console.log($SearchProducts.data)
 </script>
 
 <section>
@@ -14,6 +29,12 @@
             </li>
         {/each}
     </ul>
+    <footer>
+        {#if currentOffset > 0}
+        <a href={prevUrl} target="_self">prev</a>
+        {/if}
+        <a href={nextUrl} target="_self">next</a>
+    </footer>
 </section>
 
 <style>
@@ -27,5 +48,11 @@
         display: flex;
         flex-direction: column;
         gap: 1rem;
+    }
+    
+    footer {
+        display: flex;
+        gap: 1rem;
+        justify-content: center;
     }
 </style>
