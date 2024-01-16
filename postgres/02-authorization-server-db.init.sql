1ca579fb3fd732bbcda963408cb37e4f1c195ea5
bartosz <bartosz.socki@gmail.com>
2024-01-16 16:16:22 +0100

    refactor(auth-server): changed auth-server db schema

--- postgres/02-authorization-server-db.init.sql
+++ postgres/02-authorization-server-db.init.sql
@@ -8,27 +8,22 @@ create user authorization_server with password 'Password1!';
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
 create table if not exists authorization_server.users (
-	id serial primary key,
+	id int generated always as identity not null primary key,
 	email text not null unique,
-	password text null
+	name text null,
+	surname text null
 );
 
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-create table if not exists authorization_server.roles (
-	id serial primary key,
-	name text not null unique
-);
-
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-create table if not exists authorization_server.user_role (
-	id serial primary key,
+create table if not exists user_account (
+	id int generated always as identity not null primary key,
 	user_id int not null,
-	role_id int not null, 
+	role text not null,
+	password text not null,
+	is_account_non_locked boolean default false,
+    is_account_enabled boolean default false,
 
-	constraint fk_user_id foreign key (user_id) references authorization_server.users(id),
-	constraint fk_role_id foreign key (role_id) references authorization_server.roles(id)
+	constraint fk_user_id foreign key (user_id) references authorization_server.users(id)
 );
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
@@ -36,10 +31,3 @@ create table if not exists authorization_server.user_role (
 grant all privileges on schema authorization_server to authorization_server;
 grant all privileges on database authorization_server_db to authorization_server;
 grant all privileges on all tables in schema authorization_server to authorization_server;
-
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-insert into authorization_server.users (id, email, password) values (1, 'user', 'password');
-insert into authorization_server.roles (id, name) values (1, 'USER');
-insert into authorization_server.user_role (user_id, role_id) values (1, 1);
-
